3da435c006c7412a3d1640dd2e5089af5ba77480
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-06 00:04:16 -0300

    -Aula 06: Tipos primitivos Pt.05 - Exercício

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+import java.time.LocalDate;
+
+/*
+Prática
+
+Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o salário de <salario>, na data <data>
+ */
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Ricardo Maricato";
+        String address = "Rua Antônio Matta";
+        double wage = 4000.0;
+        String salaryReceiptDate = "06/05/2021";
+
+        String report = "Eu " + name + ", morando no endereço " + address
+                + ", confirmo que recebi o salário de " + wage + ", na data " + salaryReceiptDate;
+
+        System.out.println(report);
+    }
+}
