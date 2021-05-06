60247cbea375e4e9245bcf242e420122acbf9062
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-05 23:43:42 -0300

    -Aula 05: Tipos primitivos Pt.04 - Strings

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -6,14 +6,17 @@ public class Aula02TiposPrimitivos {
         int age = (int) 10000000000L;
         long numeroGrande = (long) 155.23;
         double salarioDouble = 2000.0D;
-        float salarioFloat = 2500.0F;
+        float salarioFloat = (float) 2500.0D;
         byte idadeByte = -128;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
+        String nome = "Goku";
+        var nome2 = "Vegeta";
 
         System.out.println(age);
         System.out.println(caractere);
         System.out.println(numeroGrande);
+        System.out.println("Meu nome é : " + nome);
     }
 }
