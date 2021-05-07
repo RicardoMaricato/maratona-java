ee5f8191f1b3ef08a2191af686dfd18aae066652
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-07 16:21:35 -0300

    -Aula 05: Estruturas Condicionais Pt.01 - IF

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (isAutorizadoComprarBebida)
+            System.out.println("Autorizado a comprar bebida alcólica");
+        // !
+        if (!isAutorizadoComprarBebida)
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+
+        System.out.println("Fora do If");
+    }
+}
