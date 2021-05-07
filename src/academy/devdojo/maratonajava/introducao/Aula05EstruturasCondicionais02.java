004f6475d3c0ca9518ce2e9df75008f06d5eb3b9
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-07 16:48:39 -0300

    -Aula 05: Estruturas Condicionais Pt.03 - Operador ternário

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -15,5 +15,9 @@ public class Aula05EstruturasCondicionais02 {
         } else {
             categoria = "Categoria Adulto";
         }
+        System.out.println(categoria);
+
+        categoria = idade < 15 ? "Categoria Infantil" : idade >= 15 && idade < 18 ? "Categoria Juvenil" : "Categoria Adulto";
+        System.out.println(categoria);
     }
 }
